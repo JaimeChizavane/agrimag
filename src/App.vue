bac7764883d78483821a5022a46784198163c8de
Maizer Gomes <maizer.gomes@gmail.com>
2021-09-16 16:53:07 +0200

    Set up initial pages

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+    <q-header/>
     <router-view/>
+    <q-footer/>
+
+
+    <!-- Back to top -->
+    <div class="btn-back-to-top" id="myBtn" @click="scroll">
+		<span class="symbol-btn-back-to-top">
+			<span class="fas fa-angle-up"></span>
+		</span>
+    </div>
+
+    <!-- Modal Video 01-->
+    <div class="modal fade" id="modal-video-01" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog" role="document" data-dismiss="modal">
+        <div class="close-mo-video-01 trans-0-4" data-dismiss="modal" aria-label="Close">&times;</div>
+
+        <div class="wrap-video-mo-01">
+          <div class="video-mo-01">
+            <iframe src="#" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import QHeader from "@/components/Header";
+import QFooter from "@/components/Footer";
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  components: { QFooter, QHeader },
+  methods: {
+    scroll() {
+      window.scroll({ top: 0, left: 0, behavior: 'smooth' })
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    meta: {
+      title: 'Inicio'
+    },
+    component: () => import('../views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/news',
+    name: 'news',
+    meta: {
+      title: 'Noticias'
+    },
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/business',
+    name: 'business',
+    meta: {
+      title: 'Negocios'
+    },
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/contacts',
+    name: 'contacts',
+    meta: {
+      title: 'Contactos'
+    },
+    component: () => import('../views/Home.vue')
   }
 ]
 
