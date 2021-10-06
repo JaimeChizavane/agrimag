873cb17090088f7920c5304b90ab33ff8e61fd7c
jaimechizavane <jaime.jcm@gmail.com>
2021-10-06 23:41:11 +0200

    Adding new

--- src/views/News.vue
+++ src/views/News.vue
@@ -16,37 +16,19 @@
 								<div class="size-w-9 w-full-sr575 m-b-25">
 									<h5 class="p-b-12">
 										<a href="blog-detail-02.html" class="f1-l-1 cl2 hov-cl10 trans-03 respon2">
-											Robot lorem ipsum dolor sit amet consectetur 
+											Ciclones arrasam capacidade produtiva de Moçambique
 										</a>
 									</h5>
 
-									<div class="cl8 p-b-18">
-										<a href="#" class="f1-s-4 cl8 hov-cl10 trans-03">
-											by John Alvarado
-										</a>
-
-										<span class="f1-s-3 m-rl-3">
-											-
-										</span>
-
-										<span class="f1-s-3">
-											Feb 18
-										</span>
-									</div>
-
 									<p class="f1-s-1 cl6 p-b-24">
-										Duis eu felis id tortor congue consequat. Sed vitae vestibulum enim, et pharetra magna
+										Os números definitivos são avassaladores! O ciclone Idai atingiu o centro de Moçambique em Março
 									</p>
-
-									<a href="blog-detail-02.html" class="f1-s-1 cl9 hov-cl10 trans-03">
-										Read More
-										<i class="m-l-2 fa fa-long-arrow-alt-right"></i>
-									</a>
+									
 								</div>
 							</div>
 
 							<!-- Item post -->
-							<div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
+							<!-- <div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
 								<a href="blog-detail-02.html" class="size-w-8 wrap-pic-w hov1 trans-03 w-full-sr575 m-b-25">
 									<img src="images/post-44.jpg" alt="IMG">
 								</a>
@@ -81,10 +63,10 @@
 										<i class="m-l-2 fa fa-long-arrow-alt-right"></i>
 									</a>
 								</div>
-							</div>
+							</div> -->
 
 							<!-- Item post -->
-							<div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
+							<!-- <div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
 								<a href="blog-detail-02.html" class="size-w-8 wrap-pic-w hov1 trans-03 w-full-sr575 m-b-25">
 									<img src="images/post-45.jpg" alt="IMG">
 								</a>
@@ -119,10 +101,10 @@
 										<i class="m-l-2 fa fa-long-arrow-alt-right"></i>
 									</a>
 								</div>
-							</div>
+							</div> -->
 
 							<!-- Item post -->
-							<div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
+							<!-- <div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
 								<a href="blog-detail-02.html" class="size-w-8 wrap-pic-w hov1 trans-03 w-full-sr575 m-b-25">
 									<img src="images/post-46.jpg" alt="IMG">
 								</a>
@@ -157,10 +139,10 @@
 										<i class="m-l-2 fa fa-long-arrow-alt-right"></i>
 									</a>
 								</div>
-							</div>
+							</div> -->
 
 							<!-- Item post -->
-							<div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
+							<!-- <div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
 								<a href="blog-detail-02.html" class="size-w-8 wrap-pic-w hov1 trans-03 w-full-sr575 m-b-25">
 									<img src="images/post-47.jpg" alt="IMG">
 								</a>
@@ -195,10 +177,10 @@
 										<i class="m-l-2 fa fa-long-arrow-alt-right"></i>
 									</a>
 								</div>
-							</div>
+							</div> -->
 
 							<!-- Item post -->
-							<div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
+							<!-- <div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
 								<a href="blog-detail-02.html" class="size-w-8 wrap-pic-w hov1 trans-03 w-full-sr575 m-b-25">
 									<img src="images/post-48.jpg" alt="IMG">
 								</a>
@@ -233,10 +215,10 @@
 										<i class="m-l-2 fa fa-long-arrow-alt-right"></i>
 									</a>
 								</div>
-							</div>
+							</div> -->
 
 							<!-- Item post -->
-							<div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
+							<!-- <div class="flex-wr-sb-s p-t-40 p-b-15 how-bor2">
 								<a href="blog-detail-02.html" class="size-w-8 wrap-pic-w hov1 trans-03 w-full-sr575 m-b-25">
 									<img src="images/post-49.jpg" alt="IMG">
 								</a>
@@ -271,11 +253,11 @@
 										<i class="m-l-2 fa fa-long-arrow-alt-right"></i>
 									</a>
 								</div>
-							</div>
+							</div> -->
 						</div>
 
 						<a href="#" class="flex-c-c size-a-13 bo-all-1 bocl11 f1-m-6 cl6 hov-btn1 trans-03">
-							Load More
+							Ver mais
 						</a>
 					</div>
 				</div>
@@ -283,84 +265,10 @@
 				<div class="col-md-10 col-lg-4 p-b-80">
 					<div class="p-l-10 p-rl-0-sr991">							
 						<!-- Subscribe -->
-						<div class="bg10 p-rl-35 p-t-28 p-b-35 m-b-50">
-							<h5 class="f1-m-5 cl0 p-b-10">
-								Subscribe
-							</h5>
-
-							<p class="f1-s-1 cl0 p-b-25">
-								Get all latest content delivered to your email a few times a month.
-							</p>
-
-							<form class="size-a-9 pos-relative">
-								<input class="s-full f1-m-6 cl6 plh9 p-l-20 p-r-55" type="text" name="email" placeholder="Email">
-
-								<button class="size-a-10 flex-c-c ab-t-r fs-16 cl9 hov-cl10 trans-03">
-									<i class="fa fa-arrow-right"></i>
-								</button>
-							</form>
-						</div>
+						
 
 						<!-- Most Popular -->
-						<div class="p-b-23">
-							<div class="how2 how2-cl4 flex-s-c">
-								<h3 class="f1-m-2 cl3 tab01-title">
-									Most Popular
-								</h3>
-							</div>
-
-							<ul class="p-t-35">
-								<li class="flex-wr-sb-s p-b-22">
-									<div class="size-a-8 flex-c-c borad-3 size-a-8 bg9 f1-m-4 cl0 m-b-6">
-										1
-									</div>
-
-									<a href="#" class="size-w-3 f1-s-7 cl3 hov-cl10 trans-03">
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit
-									</a>
-								</li>
-
-								<li class="flex-wr-sb-s p-b-22">
-									<div class="size-a-8 flex-c-c borad-3 size-a-8 bg9 f1-m-4 cl0 m-b-6">
-										2
-									</div>
-
-									<a href="#" class="size-w-3 f1-s-7 cl3 hov-cl10 trans-03">
-										Proin velit consectetur non neque
-									</a>
-								</li>
-
-								<li class="flex-wr-sb-s p-b-22">
-									<div class="size-a-8 flex-c-c borad-3 size-a-8 bg9 f1-m-4 cl0 m-b-6">
-										3
-									</div>
-
-									<a href="#" class="size-w-3 f1-s-7 cl3 hov-cl10 trans-03">
-										Nunc vestibulum, enim vitae condimentum volutpat lobortis ante
-									</a>
-								</li>
-
-								<li class="flex-wr-sb-s p-b-22">
-									<div class="size-a-8 flex-c-c borad-3 size-a-8 bg9 f1-m-4 cl0 m-b-6">
-										4
-									</div>
-
-									<a href="#" class="size-w-3 f1-s-7 cl3 hov-cl10 trans-03">
-										Proin velit justo consectetur non neque elementum
-									</a>
-								</li>
-
-								<li class="flex-wr-sb-s p-b-22">
-									<div class="size-a-8 flex-c-c borad-3 size-a-8 bg9 f1-m-4 cl0">
-										5
-									</div>
-
-									<a href="#" class="size-w-3 f1-s-7 cl3 hov-cl10 trans-03">
-										Proin velit consectetur non neque
-									</a>
-								</li>
-							</ul>
-						</div>
+						
 
 						<!--  -->
 						<div class="flex-c-s p-b-50">
@@ -370,47 +278,7 @@
 						</div>
 						
 						<!-- Tag -->
-						<div>
-							<div class="how2 how2-cl4 flex-s-c m-b-30">
-								<h3 class="f1-m-2 cl3 tab01-title">
-									Tags
-								</h3>
-							</div>
-
-							<div class="flex-wr-s-s m-rl--5">
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									Fashion
-								</a>
-
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									Lifestyle
-								</a>
-
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									Denim
-								</a>
-
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									Streetstyle
-								</a>
-
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									Crafts
-								</a>
-
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									Magazine
-								</a>
-
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									News
-								</a>
-
-								<a href="#" class="flex-c-c size-h-2 bo-1-rad-20 bocl12 f1-s-1 cl8 hov-btn2 trans-03 p-rl-20 p-tb-5 m-all-5">
-									Blogs
-								</a>
-							</div>	
-						</div>
+						
 					</div>
 				</div>
 			</div>
